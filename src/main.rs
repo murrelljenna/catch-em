76bc5aa8efeb78b77a9b5b73dfb4b4e03d884ec5
Jenna Murrell <murrelljenna@gmail.com>
2023-08-24 21:10:44 -0400

    Add character example

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,211 @@
-use bevy::prelude::*;
+use std::f32::consts::TAU;
 
-#[derive(Component)]
-struct Person;
+use bevy::{
+    gltf::{GltfMesh, GltfNode},
+    gltf::Gltf,
+    math::Vec3Swizzles,
+    prelude::*,
+    window::CursorGrabMode,
+};
+use bevy_rapier3d::prelude::*;
 
-#[derive(Component)]
-struct Name(String);
+use bevy_fps_controller::controller::*;
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
+const SPAWN_POINT: Vec3 = Vec3::new(0.0, 1.0, 0.0);
 
-pub struct HelloPlugin;
+fn main() {
+    App::new()
+        .insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 0.5,
+        })
+        .insert_resource(ClearColor(Color::hex("D4F5F5").unwrap()))
+        .insert_resource(RapierConfiguration::default())
+        .add_plugins(DefaultPlugins)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(FpsControllerPlugin)
+        .add_systems(Startup, setup)
+        .add_systems(Update, (manage_cursor, scene_colliders, display_text, respawn))
+        .run();
+}
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_systems(Startup, add_people)
-            .add_systems(Update, greet_people);
+fn setup(
+    mut commands: Commands,
+    mut window: Query<&mut Window>,
+    assets: Res<AssetServer>,
+) {
+    let mut window = window.single_mut();
+    window.title = String::from("Minimal FPS Controller Example");
+    // commands.spawn(Window { title: "Minimal FPS Controller Example".to_string(), ..default() });
+
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            illuminance: 6000.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 7.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+
+    // Note that we have two entities for the player
+    // One is a "logical" player that handles the physics computation and collision
+    // The other is a "render" player that is what is displayed to the user
+    // This distinction is useful for later on if you want to add multiplayer,
+    // where often time these two ideas are not exactly synced up
+    commands.spawn((
+        Collider::capsule(Vec3::Y * 0.5, Vec3::Y * 1.5, 0.5),
+        Friction {
+            coefficient: 0.0,
+            combine_rule: CoefficientCombineRule::Min,
+        },
+        Restitution {
+            coefficient: 0.0,
+            combine_rule: CoefficientCombineRule::Min,
+        },
+        ActiveEvents::COLLISION_EVENTS,
+        Velocity::zero(),
+        RigidBody::Dynamic,
+        Sleeping::disabled(),
+        LockedAxes::ROTATION_LOCKED,
+        AdditionalMassProperties::Mass(1.0),
+        GravityScale(0.0),
+        Ccd { enabled: true }, // Prevent clipping when going fast
+        TransformBundle::from_transform(Transform::from_translation(SPAWN_POINT)),
+        LogicalPlayer(0),
+        FpsControllerInput {
+            pitch: -TAU / 12.0,
+            yaw: TAU * 5.0 / 8.0,
+            ..default()
+        },
+        FpsController {
+            air_acceleration: 80.0,
+            ..default()
+        }
+    ));
+    commands.spawn((
+        Camera3dBundle {
+            projection: Projection::Perspective(PerspectiveProjection {
+                fov: TAU / 5.0,
+                ..default()
+            }),
+            ..default()
+        },
+        RenderPlayer(0),
+    ));
+
+    commands.insert_resource(MainScene {
+        handle: assets.load("playground.glb"),
+        is_loaded: false,
+    });
+
+    commands.spawn(TextBundle::from_section(
+        "",
+        TextStyle {
+            font: assets.load("fira_mono.ttf"),
+            font_size: 24.0,
+            color: Color::BLACK,
+        },
+    ).with_style(Style {
+        position_type: PositionType::Absolute,
+        top: Val::Px(5.0),
+        left: Val::Px(5.0),
+        ..default()
+    }));
+}
+
+fn respawn(
+    mut query: Query<(&mut Transform, &mut Velocity)>,
+) {
+    for (mut transform, mut velocity) in &mut query {
+        if transform.translation.y > -50.0 {
+            continue;
+        }
+
+        velocity.linvel = Vec3::ZERO;
+        transform.translation = SPAWN_POINT;
     }
 }
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("hello {}!", name.0);
+#[derive(Resource)]
+struct MainScene {
+    handle: Handle<Gltf>,
+    is_loaded: bool,
+}
+
+fn scene_colliders(
+    mut commands: Commands,
+    mut main_scene: ResMut<MainScene>,
+    gltf_assets: Res<Assets<Gltf>>,
+    gltf_mesh_assets: Res<Assets<GltfMesh>>,
+    gltf_node_assets: Res<Assets<GltfNode>>,
+    mesh_assets: Res<Assets<Mesh>>,
+) {
+    if main_scene.is_loaded {
+        return;
+    }
+
+    let gltf = gltf_assets.get(&main_scene.handle);
+
+    if let Some(gltf) = gltf {
+        let scene = gltf.scenes.first().unwrap().clone();
+        commands.spawn(SceneBundle {
+            scene,
+            ..default()
+        });
+        for node in &gltf.nodes {
+            let node = gltf_node_assets.get(&node).unwrap();
+            if let Some(gltf_mesh) = node.mesh.clone() {
+                let gltf_mesh = gltf_mesh_assets.get(&gltf_mesh).unwrap();
+                for mesh_primitive in &gltf_mesh.primitives {
+                    let mesh = mesh_assets.get(&mesh_primitive.mesh).unwrap();
+                    commands.spawn((
+                        Collider::from_bevy_mesh(mesh, &ComputedColliderShape::TriMesh).unwrap(),
+                        RigidBody::Fixed,
+                        TransformBundle::from_transform(node.transform),
+                    ));
+                }
+            }
+        }
+        main_scene.is_loaded = true;
+    }
+}
+
+fn manage_cursor(
+    btn: Res<Input<MouseButton>>,
+    key: Res<Input<KeyCode>>,
+    mut window_query: Query<&mut Window>,
+    mut controller_query: Query<&mut FpsController>,
+) {
+    let mut window = window_query.single_mut();
+    if btn.just_pressed(MouseButton::Left) {
+        window.cursor.grab_mode = CursorGrabMode::Locked;
+        window.cursor.visible = false;
+        for mut controller in &mut controller_query {
+            controller.enable_input = true;
+        }
+    }
+    if key.just_pressed(KeyCode::Escape) {
+        window.cursor.grab_mode = CursorGrabMode::None;
+        window.cursor.visible = true;
+        for mut controller in &mut controller_query {
+            controller.enable_input = false;
         }
     }
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-fn main() {
-    App::new()
-        .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-        .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
-        .add_systems(Startup, add_people)
-        .add_systems(Update, greet_people)
-        .run();
+fn display_text(
+    mut controller_query: Query<(&Transform, &Velocity)>,
+    mut text_query: Query<&mut Text>,
+) {
+    for (transform, velocity) in &mut controller_query {
+        for mut text in &mut text_query {
+            text.sections[0].value = format!(
+                "vel: {:.2}, {:.2}, {:.2}\npos: {:.2}, {:.2}, {:.2}\nspd: {:.2}",
+                velocity.linvel.x, velocity.linvel.y, velocity.linvel.z,
+                transform.translation.x, transform.translation.y, transform.translation.z,
+                velocity.linvel.xz().length()
+            );
+        }
+    }
 }
